10e74bdcf753efa286db7a91cc4d6d77088e93fb
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2024-02-15 17:00:53 -0500

    finalized everything but the frontend

--- src/main/java/Nate/PillScanner/Security/SecurityConfig.java
+++ src/main/java/Nate/PillScanner/Security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-
+                .csrf().disable()
                 .authorizeRequests()
                 .requestMatchers("/login","/error", "/logout", "/nurse/createNurse").permitAll()
                 .anyRequest().hasRole("USER")
